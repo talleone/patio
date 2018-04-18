76445f509f55a454b6cc3a02cd13385208df0fe7
talleone <36417916+talleone@users.noreply.github.com>
2018-04-18 16:39:35 -0500

    Update seed_sample_data.js
    localhost to goldcurbs

--- server/scripts/seed_sample_data.js
+++ server/scripts/seed_sample_data.js
@@ -26,7 +26,7 @@ const {
   encodeTimestampedPayload
 } = require('../system/submit_utils')
 
-const SERVER = process.env.SERVER || 'http://localhost:3000'
+const SERVER = process.env.SERVER || 'http://goldcurbs.com:3000'
 const DATA = process.env.DATA
 if (DATA.indexOf('.json') === -1) {
   throw new Error('Use the "DATA" environment variable to specify a JSON file')
